a42037af2ba6038ec3ae1c4af4d5a79e7368af64
jason taekyun Kim <saurus234@gmail.com>
2022-11-25 21:25:57 +0900

    DB 연결

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: '',
-  user: 'jason',
+  host: 'database-1.c1cdb2pwiahs.ap-northeast-2.rds.amazonaws.com',
+  user: 'jasonkim',
   password: 'kim97269726',
   database: 'blockExtension',
   dialect: 'mysql',
