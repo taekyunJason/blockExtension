0896af52f47523a0654a63b88cbaa9241f1e24ee
jason taekyun Kim <saurus234@gmail.com>
2022-12-03 20:32:09 +0900

    RDS 인스턴스 재연결

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: 'database-1.c1cdb2pwiahs.ap-northeast-2.rds.amazonaws.com',
-  user: 'jasonkim',
+  host: 'database-1.cvqhsdd04tgk.ap-northeast-2.rds.amazonaws.com',
+  user: 'admin',
   password: 'kim97269726',
   database: 'blockExtension',
   dialect: 'mysql',
